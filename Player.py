665ee3959aa079f9903e5470c172b39d1419e9ce
greyhayv <lias.westen@gmail.com>
2023-06-22 20:14:24 +0200

    changed movement

--- Player.py
+++ Player.py
@@ -8,11 +8,32 @@ class Player(Entity):
         self.invincible: bool = False
         self.maxHealth = 3
         self.health = self.maxHealth
-        self.controlAngles: dict[int, int] = { pygame.K_w: 0, pygame.K_d: 90, pygame.K_s: 180, pygame.K_a: 270, pygame.K_UP: 0, pygame.K_RIGHT: 90, pygame.K_DOWN: 180, pygame.K_LEFT: 270 }
+        self.controlAngles: dict[int, list[int]] = { 0: [ pygame.K_w, pygame.K_UP ], 180: [ pygame.K_s, pygame.K_DOWN ], 90: [ pygame.K_d, pygame.K_RIGHT ], 270: [ pygame.K_a, pygame.K_LEFT ] }
 
     def update(self, time: int):
         keys = pygame.key.get_pressed()
         finalAngle: int = -1
+        for angle in self.controlAngles:
+            isPressed: bool = False
+            for key in self.controlAngles[angle]:
+                if(keys[key]):
+                    isPressed = True
+                    break
+            if(not isPressed):
+                continue
+            if(finalAngle < 0):
+                finalAngle = angle
+                continue
+            if(abs(finalAngle - angle) == 180):
+                finalAngle = -1
+                continue
+            if(abs(finalAngle - angle) == 270):
+                finalAngle = 325
+                continue
+            if(finalAngle == 45):
+                finalAngle = 0
+                continue
+            finalAngle = min(finalAngle, angle) + 45
 
         if(finalAngle >= 0):
             self.x += math.sin(math.radians(finalAngle)) * self.speed * time
