bd316f5264789eec32694dd658648ee743d1b711
greyhayv <lias.westen@gmail.com>
2023-06-23 14:45:39 +0200

    changed movement

--- Player.py
+++ Player.py
@@ -1,6 +1,7 @@
 import pygame, math
 from threading import Timer
 from Entity import Entity
+from Point import Point
 
 class Player(Entity):
     def __init__(self):
@@ -8,36 +9,34 @@ class Player(Entity):
         self.invincible: bool = False
         self.maxHealth = 3
         self.health = self.maxHealth
-        self.controlAngles: dict[int, list[int]] = { 0: [ pygame.K_w, pygame.K_UP ], 180: [ pygame.K_s, pygame.K_DOWN ], 90: [ pygame.K_d, pygame.K_RIGHT ], 270: [ pygame.K_a, pygame.K_LEFT ] }
 
     def update(self, time: int):
         keys = pygame.key.get_pressed()
-        finalAngle: int = -1
-        for angle in self.controlAngles:
-            isPressed: bool = False
-            for key in self.controlAngles[angle]:
-                if(keys[key]):
-                    isPressed = True
-                    break
-            if(not isPressed):
-                continue
-            if(finalAngle < 0):
-                finalAngle = angle
-                continue
-            if(abs(finalAngle - angle) == 180):
-                finalAngle = -1
-                continue
-            if(abs(finalAngle - angle) == 270):
-                finalAngle = 325
-                continue
-            if(finalAngle == 45):
-                finalAngle = 0
-                continue
-            finalAngle = min(finalAngle, angle) + 45
+        directionPoint: Point = Point(0, 0)
+        if(keys[pygame.K_w] or keys[pygame.K_UP]):
+            directionPoint.y += 1
+        if(keys[pygame.K_d] or keys[pygame.K_RIGHT]):
+            directionPoint.x += 1
+        if(keys[pygame.K_s] or keys[pygame.K_DOWN]):
+            directionPoint.y -= 1
+        if(keys[pygame.K_a] or keys[pygame.K_LEFT]):
+            directionPoint.x -= 1
 
-        if(finalAngle >= 0):
-            self.x += math.sin(math.radians(finalAngle)) * self.speed * time
-            self.y -= math.cos(math.radians(finalAngle)) * self.speed * time
+        if not directionPoint.equals(Point(0, 0)):
+            angle: int = 0
+            if(directionPoint.x == 0):
+                angle = 0 if directionPoint.y == 1 else 180
+            elif(directionPoint.y == 0):
+                angle = 90 if directionPoint.x == 1 else 270
+            else:
+                if(directionPoint.y > 0):
+                    angle = math.degrees(math.atan(directionPoint.y / directionPoint.x))
+                else:
+                    angle = 180 + math.degrees(math.atan(directionPoint.y / directionPoint.x))
+                if(angle < 0):
+                    angle = 360 + angle
+            self.x += math.sin(math.radians(angle)) * self.speed * time
+            self.y -= math.cos(math.radians(angle)) * self.speed * time
 
         super().update()
 
