f888f360acb28b30bb18bdf6c70def3348a89504
greyhayv <lias.westen@gmail.com>
2023-06-25 00:44:19 +0200

    added collision to water

--- LevelObjects/Water.py
+++ LevelObjects/Water.py
@@ -1,3 +1,4 @@
+import pygame
 from LevelObject import LevelObject
 from Point import Point
 
@@ -8,3 +9,4 @@ class Water(LevelObject):
 
     def __init__(self, pos: Point):
         super().__init__(pos)
+        self.collisionRect = pygame.Rect(0, 0, 50, 50)
